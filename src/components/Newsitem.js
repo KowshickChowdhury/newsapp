a48f49b1b582438ea99a84e9f7c404a8007231b0
KowshickChowdhury <kowshick1170@gmail.com>
2023-09-10 01:46:43 +0600

    working with News api

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
 
   render() {
-    let {title,description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
 
     return (
-      <div>
+      <div className='my-3'>
             <div className="card" style={{width: "18rem"}}>
-            <img src="https://cdn.vox-cdn.com/thumbor/fIDI3vu2St6anUocTgKndOUnqq8=/416x192:2507x1583/1200x628/filters:focal(1440x900:1441x901)/cdn.vox-cdn.com/uploads/chorus_asset/file/24903809/google_pixel_8.png" className="card-img-top" alt="..." />
+            <img src={imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read more</a>
             </div>
             </div>
       </div>
